0c8b91dc9155a2b4e69da05028d9e89cd27e704e
junwookim <junwoo0824@gmail.com>
2023-05-24 09:24:05 +0900

    refactor splitAndSum2 function

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,6 +4,34 @@
 using std::cout;
 using std::string;
 using std::vector;
+
+vector<string> split(string& text)
+{
+	vector<string> values;
+	text += "-";
+	// split
+	int a = 0, b = 0;
+	while (1) {
+		b = text.find("-", a);
+		if (b == -1) break;
+		string temp = text.substr(a, b - a);
+		values.push_back(temp);
+		a = b + 1;
+	}
+	return values;
+}
+
+int sum_values(vector<string>& values)
+{
+	int result = 0;
+	// sum 
+	for (int i = 0; i < values.size(); i++) {
+		int temp = stoi(values[i]);
+		result += temp;
+	}
+	return result;
+}
+
 int splitAndSum2(string text)
 {
 	int result = 0;
@@ -12,22 +40,9 @@ int splitAndSum2(string text)
 		result = 0;
 	}
 	else {
-		vector<string> values;
-		text += "-";
-		// split
-		int a = 0, b = 0;
-		while (1) {
-			b = text.find("-", a);
-			if (b == -1) break;
-			string temp = text.substr(a, b - a);
-			values.push_back(temp);
-			a = b + 1;
-		}
-		// sum 
-		for (int i = 0; i < values.size(); i++) {
-			int temp = stoi(values[i]);
-			result += temp;
-		}
+		
+		vector<string> values = split(text);
+		result = sum_values(values);
 	}
 	return result;
 }
