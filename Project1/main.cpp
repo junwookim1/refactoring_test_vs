ffb58e64f480b4b37e822e9506a4dd525111535b
junwookim <junwoo0824@gmail.com>
2023-05-24 09:15:16 +0900

    Modify to SplitAndSum code

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,124 +1,38 @@
 #include<iostream>
 #include<vector>
-using namespace std;
-
-class Sort
+#include<string>
+using std::cout;
+using std::string;
+using std::vector;
+int splitAndSum2(string text)
 {
-public:
-	virtual void sort(vector<int>& arr) = 0;
-};
-
-class BubbleSort : public Sort
-{
-public:
-	void sort(vector<int>& arr) override
+	int result = 0;
+	if (text.empty())
 	{
-		for (int a = 0; a < arr.size(); a++)
-		{
-			for (int b = 0; b < arr.size() - 1 - a; b++)
-			{
-				if (arr[b] > arr[b + 1])
-				{
-					std::swap(arr[b], arr[b + 1]);
-				}
-			}
+		result = 0;
+	}
+	else {
+		vector<string> values;
+		text += "-";
+		// split
+		int a = 0, b = 0;
+		while (1) {
+			b = text.find("-", a);
+			if (b == -1) break;
+			string temp = text.substr(a, b - a);
+			values.push_back(temp);
+			a = b + 1;
+		}
+		// sum 
+		for (int i = 0; i < values.size(); i++) {
+			int temp = stoi(values[i]);
+			result += temp;
 		}
 	}
-};
-
-class SelectionSort : public Sort
-{
-public:
-	void sort(vector<int>& arr) override
-	{
-		for (int a = 0; a < arr.size(); a++)
-		{
-			for (int b = a + 1; b < arr.size(); b++)
-			{
-				if (arr[a] > arr[b])
-				{
-					std::swap(arr[a], arr[b]);
-				}
-			}
-		}
-	}
-};
-
-class SortMachine
-{
-public:
-	void insert(int val)
-	{
-		arr.push_back(val);
-	}
-
-	void selection(string name)
-	{
-		if (m_sort != nullptr)
-		{
-			delete m_sort;
-			m_sort = nullptr;
-		}
-		m_sort = createSort(name);
-
-	}
-	void run()
-	{
-		if (m_sort == nullptr) return;
-		m_sort->sort(arr);
-	}
-	void show()
-	{
-		for (const int& val : arr)
-		{
-			cout << val << " ";
-		}
-		cout << endl;
-	}
-private:
-	Sort* createSort(string name)
-	{
-		if (name == "bubble")
-		{
-			return new BubbleSort();
-		}
-		if (name == "selection")
-		{
-			return new SelectionSort();
-		}
-		return nullptr;
-	}
-private:
-	
-	vector<int> arr;
-	Sort* m_sort;
-};
-
-
-void print(vector<int>& arr)
-{
-	for (const int& val : arr)
-	{
-		cout << val << " ";
-	}
-	cout << endl;
+	return result;
 }
-
-
 int main()
 {
-
-	SortMachine* mixer = new SortMachine();
-	mixer->insert(3);
-	mixer->insert(7);
-	mixer->insert(1);
-	mixer->insert(5);
-	mixer->insert(2);
-
-	mixer->selection("selection");
-	mixer->run();
-	mixer->show();
-
-
+	cout << splitAndSum2("100-10-20");
 	return 0;
 }
