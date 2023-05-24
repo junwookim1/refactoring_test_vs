f113c78fac77934a0c1d31b71ba95aacbe212667
junwookim <junwoo0824@gmail.com>
2023-05-24 11:29:07 +0900

    Change to other example

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,53 +1,66 @@
-#include<iostream>
-#include<vector>
-#include<string>
-using std::cout;
-using std::string;
-using std::vector;
-
-vector<string> split(string& text)
-{
-	vector<string> values;
-	text += "-";
-	
-	size_t a = 0;
-	size_t b = 0;
-	while (true)
-	{
-		b = text.find("-", a);
-		if (b == -1) break;
-		string temp = text.substr(a, b - a);
-		values.push_back(temp);
-		a = b + 1;
-	}
-	return values;
-}
-
-int get_sum(const vector<string>& values)
-{
-	int result = 0;
-	
-	for (auto& value : values)
-	{
-		const int temp = stoi(value);
-		result += temp;
-	}
-	return result;
-}
-
-int split_and_sum2(string text)
-{
-	if (text.empty())
-	{
-		return 0;
-	}
-
-	const vector<string> values = split(text);
-	return get_sum(values);
-}
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	cout << split_and_sum2("100-10-20");
+	//25+61=100
+	//1 ~ 5자리수 덧셈 수식이 맞는지 확인하는 프로그램
+	//띄어쓰기 없음
+
+	string str = "25+61=86"; //PASS
+	//string str = "12345+12345=24690"; //PASS
+	//string str = "5++5=10"; //ERROR
+	//string str = "10000+1=10002"; //FAIL
+
+	int flag1 = 0;
+	int cnt1 = 0;
+	int cnt2 = 0;
+	int p1, p2 = 0;
+	//+와 = 개수 확인
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == '+') {
+			cnt1++;
+			p1 = i;
+		}
+		else if (str[i] == '=') {
+			cnt2++;
+			p2 = i;
+		}
+		else if (!(str[i] >= '0' && str[i] <= '9')) {
+			flag1 = 1;
+			break;
+		}
+	}
+
+	int flag2 = 0;
+	if (cnt1 == 1 && cnt2 == 1 && p1 < p2 && flag1 == 0) {
+		if (p1 >= 1 && p2 >= 3 && p2 < str.length() - 1) {
+
+			flag2 = 1;
+			string num1 = str.substr(0, p1);
+			string num2 = str.substr(p1 + 1, p2 - p1 - 1);
+			string num3 = str.substr(p2 + 1);
+
+			int n1 = stoi(num1);
+			int n2 = stoi(num2);
+			int n3 = stoi(num3);
+
+			if (n1 + n2 == n3) {
+				cout << "PASS";
+			}
+			else cout << "FAIL";
+		}
+	}
+	else {
+		cout << "ERROR";
+		flag2 = 1;
+	}
+
+	if (flag2 == 0) {
+		cout << "ERROR";
+	}
+
 	return 0;
 }
